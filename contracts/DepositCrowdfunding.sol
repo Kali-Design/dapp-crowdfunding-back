05cad0950df9586844ce0803794cc26714af3ca9
Kali-Design <hm_leo@hotmail.com>
2021-01-15 17:51:09 +0100

    Finish

--- contracts/DepositCrowdfunding.sol
+++ contracts/DepositCrowdfunding.sol
@@ -2,11 +2,16 @@
 pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
+/// @title Dapp de crowdfunding en Ether pour definir la structure et les fonctions de ce Smart Contract
+/// @author Henri-Michel LEOCADIE
+/// @notice Vous ne pouvez utliser ce contrat que pour faire des dépots et retour éventuel du ou des dépôts la plus basique
+/// @dev Tous les appels de fonction sont implémentés sans effects secondaires
+
 contract DepositCrowdfunding {
     using SafeMath for uint256;
 
     mapping(address => uint256) private _balances;
-
+/// Fonction pour dépôt d'Ether avec sa balance
     function deposit() public payable {
         _balances[msg.sender] = _balances[msg.sender].add(msg.value);
     }
@@ -15,14 +20,14 @@ contract DepositCrowdfunding {
         return _balances[addr];
     }
 
-    
+/// Fonction de transfert d'Ether avec sa balance
     function transfer(address dst, uint256 amount) public {
         require(_balances[msg.sender] >= amount, "DepositCrowdfunding: Not enough funds for transfer");
         _balances[dst] = _balances[dst].add(amount);
         _balances[msg.sender] = _balances[msg.sender].sub(amount);
     }
 
-    // 
+/// Fonction de récupération de l'Ether avec sa balance
     function withdraw(uint _amount, address payable _to) public {
         require(_balances[msg.sender] > 0, "DepositCrowdfunding: 0 ethers in wallet");
 
@@ -30,7 +35,7 @@ contract DepositCrowdfunding {
 
         
         //_balances[msg.sender] = 0;
-    
+ /// Appel de la récupération de l'Ether dans sa balance   
         (bool success, ) = _to.call{value: _amount}(""); 
         require(success,"failed to transfer the funds");
     }
