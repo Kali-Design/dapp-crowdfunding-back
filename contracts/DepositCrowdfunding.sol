94b1f727245b33a0b05875118d771e2fe3f9ab3f
Kali-Design <hm_leo@hotmail.com>
2021-01-19 14:11:10 +0100

    working progress

--- contracts/DepositCrowdfunding.sol
+++ contracts/DepositCrowdfunding.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2; // contract address :"0x22bF4B689F63ED71F701Ce59713D87fE0fd795b7"
+
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 /// @title Dapp de crowdfunding en Ether pour definir la structure et les fonctions de ce Smart Contract
@@ -8,10 +10,13 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 /// @dev Tous les appels de fonction sont implémentés sans effects secondaires
 
 contract DepositCrowdfunding {
+    
     using SafeMath for uint256;
-
+    uint256 Amount;
+    address Addr;
+    
     mapping(address => uint256) private _balances;
-/// Fonction pour dépôt d'Ether avec sa balance
+
     function deposit() public payable {
         _balances[msg.sender] = _balances[msg.sender].add(msg.value);
     }
@@ -20,22 +25,41 @@ contract DepositCrowdfunding {
         return _balances[addr];
     }
 
-/// Fonction de transfert d'Ether avec sa balance
-    function transfer(address dst, uint256 amount) public {
+    
+    /*function transfer(address dst, uint256 amount) public {
         require(_balances[msg.sender] >= amount, "DepositCrowdfunding: Not enough funds for transfer");
+        Amount = amount;
         _balances[dst] = _balances[dst].add(amount);
         _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        
+    }*/
+    
+    function transfer(address _recipient, uint256 _amount) public returns (bool) {
+        require(_balances[msg.sender] >= _amount, 'transfer amount exceeds balance');
+        _balances[msg.sender] = _balances[msg.sender].sub(_amount);
+        _balances[_recipient] = _balances[_recipient].add(_amount);
+        emit Transfer(msg.sender, _recipient, _amount);
+        return true;
+
     }
 
-/// Fonction de récupération de l'Ether avec sa balance
-    function withdraw(uint _amount, address payable _to) public {
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    
+    function withdraw(address addr, uint _amount, address payable _to) public {
         require(_balances[msg.sender] > 0, "DepositCrowdfunding: 0 ethers in wallet");
-
+        Addr = addr;
          _balances[msg.sender] = _balances[msg.sender].sub(_amount);
 
+        
+        //_balances[msg.sender] = 0;
     
- /// Appel de la récupération de l'Ether dans sa balance   
         (bool success, ) = _to.call{value: _amount}(""); 
         require(success,"failed to transfer the funds");
     }
+    
+    function getAmount() public view returns (uint256) {
+        return Amount;
+    }
+    
+    
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line no-unused-vars
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -7,6 +9,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  // eslint-disable-next-line comma-dangle
   document.getElementById('root')
 );
 
