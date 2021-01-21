6723c33744352ee2b71fc1373b6c5ab8cbc4cd3d
Kali-Design <hm_leo@hotmail.com>
2021-01-21 01:26:47 +0100

    working progress

--- contracts/DepositCrowdfunding.sol
+++ contracts/DepositCrowdfunding.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
-pragma experimental ABIEncoderV2; // contract address :"0x22bF4B689F63ED71F701Ce59713D87fE0fd795b7"
+pragma solidity ^0.6.0; // Langue de compilation
+pragma experimental ABIEncoderV2; // Encoder pour éviter les bugs dans la Blockchain
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol"; // Librairie Openzepplin de sécurisation échanges d'argents numériques
 
 /// @title Dapp de crowdfunding en Ether pour definir la structure et les fonctions de ce Smart Contract
 /// @author Henri-Michel LEOCADIE
@@ -11,19 +11,34 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
 contract DepositCrowdfunding {
-    
+
+/// @info code : SafeMath librairie d'Openzepplin qui sécurise tous les échanges d'argents numériques 
+/// dans le Blockchain   
     using SafeMath for uint256;
-    
+
+/// @info code : "mapping" est une variable qui permet d'associer deux variables entre elles,
+/// cependant ici, elle gére les adresses et les montants
+
  mapping(address => uint256) public balances;
+
+/// @info code : "Event" pour la gestion des montants versés lors du Deposit, le Withdraw et le transfert ainsi
+/// qu'un call pour rappeller l'adresse ou les adresses avec leurs montants.
 
     event Deposit(address sender, uint256 amount);
     event Withdrawal(address receiver, uint256 amount);
     event Transfer(address sender, address receiver, uint256 amount);
 
+ /// @info code : le .add utilise bien la sécurité du SafeMath de la bibliothéque d'Openzepplin
+ /// @info code : la fonction "deposit" est pour déposer de l'Ether dans le contrat déployer avec sa balance
+
  function deposit() public payable {
         emit Deposit(msg.sender, msg.value);
         balances[msg.sender] = balances[msg.sender].add(msg.value);
     }
+/// @info code : la fonction "withdraw" est pour rembourser simplement à partir du Front
+/// @info code : le "require" est pour vérifier que l'argent est bien sur le compte avant le remboursement
+/// @info code : "Emit" fait un appel à "Event Withdrawal" et sa envoi à l'adresse du "receiver" un montant, ensuite la 
+/// balance de celui qui envoie l'argent soustraire le montant puis l'envoi au "receiver"
 
     function withdraw(uint256 amount) public { 
         require(balances[msg.sender] >= amount, "Insufficient funds");
@@ -31,20 +46,23 @@ contract DepositCrowdfunding {
         emit Withdrawal(msg.sender, amount);
         msg.sender.transfer(amount);
     }
+/// @info code : le .sub et le .add utilise bien la sécurité du SafeMath de la bibliothéque d'Openzepplin
+/// @info code : La fonction "transfer" permet d'envoyer de l'argent sur un autre compte en superAdmin pour rembourser
+/// de manière indépendante et dispatcher sur différentes comptes
 
-    function transfer(address receiver, uint256 amount) public { // SafeMath 
+    function transfer(address receiver, uint256 amount) public { 
         require(balances[msg.sender] >= amount, "Insufficient funds");
         balances[msg.sender] = balances[msg.sender].sub(amount);
         balances[receiver] = balances[receiver].add(amount);
         emit Transfer(msg.sender, receiver, amount);
     }
 
-        // Développement futur 
+/// @ info code : Développement futur de la dapp pour intégrer et mémoriser plusieurs adresses en même temps dans le Front
     /*function transfer(address[] memory receivers, uint amount) public {
         require(balances[msg.sender] >= receivers.length * amount, "Insufficient funds");
         for (uint i=0; i<receivers.length; i++) {
-            balances[msg.sender] -= amount; //laisser SafeMath faire son travaille 
-            balances[receivers[i]] += amount; //laisser SafeMath faire son travaille
+            balances[msg.sender] -= amount; 
+            balances[receivers[i]] += amount; 
              emit Transfer(msg.sender, receivers[i], amount);*
         }
     }*/
--- test/DepositCrowdfunding.test.js
+++ test/DepositCrowdfunding.test.js
@@ -6,26 +6,42 @@ import { accounts, contract } from '@openzeppelin/test-environment';
 import { expect } from 'chai';
 
 // eslint-disable-next-line no-unused-vars
-const [ owner, deposit ] = accounts;
 
 const DepositCrowdfunding = contract.fromArtifact('DepositCrowdfunding');
 
+
 describe('DepositCrowdfunding', function () {
-  it('deployer is owner', async function () {
-    const owner = await DepositCrowdfunding.new({ from: owner });
-    expect(await deposit.owner()).to.equal(owner);
-  });
-  it('deployer is deposit', async function () {
-    // eslint-disable-next-line no-unused-vars
-    const deposit = await DepositCrowdfunding.new({ from: owner });
-    expect(await deposit.owner()).to.equal(owner);
-  // eslint-disable-next-line padded-blocks
-  });
+  this.timout(0);
 
-  it('deployer is withdraw', async function () {
-    const withdraw = await DepositCrowdfunding.new({ from: owner });
-    expect(await withdraw.owner()).to.equal(owner);
-  // eslint-disable-next-line padded-blocks
-  });
+  const [owner, dev] = accounts;
+  const MESSAGE = 'WELCOME IN THE BLOCKCHAIN';
+  const _MESSAGE = 'NEW MESSAGE';
 
-});
+  context('DepositCrowdfunding initial state', function () {
+    beforeEach(async function () {
+      this.depositCrowdfunding = await DepositCrowdfunding.new(owner, MESSAGE, { from: dev });
+    })
+
+    it(`has message ${MESSAGE}`, async function () {
+      expect(await this.depositCrowdfunding.getMessage()).to.equal(MESSAGE);
+
+    })
+
+    it('has owner', async function () {
+      expect(await this.depositCrowdfunding.owner()).to.equal(owner);
+    });
+
+    context('DepositCrowdfunding functions', function () {
+      beforeEach(async function () {
+        this.depositCrowdfunding = await DepositCrowdfunding.new(owner, MESSAGE, { from: dev });
+      });
+      it('handles not finished yet', async function () {
+        expect(await this.depositCrowdfunding.goodbye({ from: dev })).to.equal('not finished yet!!');
+      });
+
+      it('handles finished', async function () {
+        await time.increase(time.duration.weeks(24));
+        expect(await this.depositCrowdfunding.goodbye({ from: dev })).to.equal('congratulations and goodbye!!');
+      });
+    });
+  });
